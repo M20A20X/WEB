e5a23a910aebf53d95766fb72dcc7f8de0c6268d
Maksym Shapovalov <maks0681912507@gmail.com>
2022-05-27 01:03:36 +0300

    Complete DebugMVC task

---
+++ LW7/DebugMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace DebugMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 5;
+            int result = firstVal / secondVal;
+
+            ViewBag.Message = "Отладка приложения ASP.NET MVC!";
+
+            return View(result);
+        }
+    }
+}
